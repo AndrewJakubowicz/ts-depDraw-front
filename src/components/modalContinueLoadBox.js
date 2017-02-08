97159ae6a6f822501fe6537b76083fa36d9ea459
SpyR1014 <spyr1014@gmail.com>
2017-02-08 23:31:19 +1100

    added modal load dialog

--- src/components/modalContinueLoadBox.js
+++ src/components/modalContinueLoadBox.js
@@ -7,19 +7,54 @@ import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 
+
+
 /**
  * This dialog can only be closed if an action is selected.
  */
 class DialogStartModalComponent extends React.Component {
     state = {
-        open: true
+        open: true,
+        loadDisabled: !(!(window.File && window.FileReader && window.FileList && window.Blob)),
+        warningText: !(!(window.File && window.FileReader && window.FileList && window.Blob)) ? "" : "Your browser doesn't support loading files!"
     };
 
+    handleFileChange(e){
+        // Here we make sure the saved file is legitimate.
+        var file = e.target.files[0];
+        if (file && !(!(window.File && window.FileReader && window.FileList && window.Blob))) {
+            if (file.name.slice(-9) === "tsDepDraw"){
+                let reader = new FileReader();
+
+                reader.readAsText(file);
+                reader.onload = e => {
+                    const fileText = e.target.result;
+                    try {
+                        this.setState({warningText: ""});
+                        this.setState({loadDisabled: false})
+                        const fileParsed = JSON.parse(fileText);
+                    } catch(e) {
+                        this.setState({warningText: "Your saved file is corrupt"});
+                        this.setState({loadDisabled: true})
+                    }
+                }
+            } else {
+                this.setState({warningText: "Make sure your save file ends with '.tsDepDraw'"});
+            }
+        } else {
+            this.setState({loadDisabled: true})
+        }
+        
+    }
+    shouldComponentUpdate = () => {
+        return true;
+    }
     handleOpen = () => {
         this.setState({open: true});
     };
 
-    handleClose = () => {
+    handleCloseWithLoad = () => {
+
         this.setState({open: false});
     };
 
@@ -31,9 +66,10 @@ class DialogStartModalComponent extends React.Component {
     render() {
         const actions = [
             <FlatButton
-                label="Load previous diagram"
-                primary={false}
-                onTouchTap={this.handleClose}
+                label="Load diagram from file"
+                primary={!this.state.loadDisabled}
+                disabled={this.state.loadDisabled}
+                onTouchTap={this.handleCloseWithLoad}
             />,
             <FlatButton
                 label="Continue"
@@ -49,6 +85,12 @@ class DialogStartModalComponent extends React.Component {
                 open={this.state.open}
                 >
                 Would you like to continue to a live server, or load a saved diagram?
+                <br />
+                <span style={{color: "red"}}>{this.state.warningText ? <span><br />{this.state.warningText}<br /></span> : ""}</span>
+                <br />
+                <input id="file" type="file" accept="tsDepDraw"
+                    onChange={e => this.handleFileChange(e)}
+                />
             </Dialog>
         )
     }
